d754de5f1919471747106a168c522eba6abd49b6
Yudiantara11 <antarayudi568@gmail.com>
2022-04-12 17:24:38 +0800

    sudah bisa mengganti warna dengan urutan index

--- uts.js
+++ uts.js
@@ -5,4 +5,17 @@ document.onmousemove = (event) => {
     var y = event.clientY;
     box.style.left = x - (box.clientWidth/2) + "px";
     box.style.top = y - (box.clientWidth/2) + "px";
-}
+    }
+   
+    const btn = document.getElementById('box');
+
+    let index = 0;
+    
+    const colors = ['red', 'white', 'purple', 'black'];
+    
+    box.addEventListener('click', function onClick() {
+      box.style.backgroundColor = colors[index];
+      box.style.color = 'white';
+    
+      index = index >= colors.length - 1 ? 0 : index + 1;
+    });
