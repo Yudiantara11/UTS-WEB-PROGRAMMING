d840970a481e3529a6c1db255887b463743b8b21
Yudiantara11 <antarayudi568@gmail.com>
2022-04-12 17:35:15 +0800

    color background sudah random

--- uts.js
+++ uts.js
@@ -6,16 +6,17 @@ document.onmousemove = (event) => {
     box.style.left = x - (box.clientWidth/2) + "px";
     box.style.top = y - (box.clientWidth/2) + "px";
     }
-   
-    const btn = document.getElementById('box');
-
-    let index = 0;
-    
-    const colors = ['red', 'white', 'purple', 'black'];
-    
-    box.addEventListener('click', function onClick() {
-      box.style.backgroundColor = colors[index];
-      box.style.color = 'white';
-    
-      index = index >= colors.length - 1 ? 0 : index + 1;
-    });
+function random(number){
+    return Math.floor(Math.random() * number)
+}
+document.onclick = function () {
+berubah= "rgb("+random(255)+", "+random(255)+", "+random(255)+")";
+box.style.backgroundColor = berubah;
+}
+function random(number){
+    return Math.floor(Math.random() * number)
+}
+document.onclick = function () {
+randomize= "rgb("+random(255)+", "+random(255)+", "+random(255)+")";
+box.style.backgroundColor = randomize;
+}
